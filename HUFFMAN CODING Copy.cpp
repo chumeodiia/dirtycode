5d9e1ab562ff64058c4f2fe1108fb26e37b8f103
chumeodiia <duytuandao31@gmail.com>
2022-01-24 07:43:10 +0700

    edit file

--- HUFFMAN CODING Copy.cpp
+++ HUFFMAN CODING Copy.cpp
@@ -14,49 +14,49 @@ void solve()
 	{
 		long long num_1 = dt.top().fi, v1 = dt.top().se; dt.pop();
 		long long num_2 = dt.top().fi, v2 = dt.top().se; dt.pop();
-		
+
 		dem++;
 		a[dem].push_back(v1);
 		a[dem].push_back(v2);
-		
+
 		dt.push({num_1 + num_2,dem});
 	}
 }
 void DFS(int u,int count)
 {
 	if(a[u].size()==0) result[t[u]] = count;
-	
+
 	for(int k=0;k < a[u].size();k++) DFS(a[u][k],count+1);
 }
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	
-	
+
+
 	cin>>n>>s;
 	n = s.length()-1;
-	
+
 	for(int i=0;i<=n;i++)
 	{
 		if(d[s[i]]==0) t += s[i];
 		d[s[i]]++;
 	}
-	// all element in array is same as one 
-	if(t.length()==1) 
+	// all element in array is same as one
+	if(t.length()==1)
 	{
 		cout<<s.length();
 		return 0;
 	}
 	// solving problem
 	for(int i=0;i<=t.length()-1;i++) dt.push({d[t[i]],i});
-	
+
 	dem = t.length()-1;
 	solve();
-	
+
 	DFS(dt.top().se,0);
-	
+
 	for(int i=0;i<=n;i++) res += result[s[i]];
-	
-	cout<<res;
+
+    cout<<"conbooyuran";
 	return 0;
 }
